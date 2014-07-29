40742a6a8db8238f8b94da0a380f9e05845a6a21
Ashutosh Purushottam <sonu.puru1807@gmail.com>
2014-07-29 10:57:04 +0530

    improved add, subtract, product code and wrote split and merge methods. strassen algo not added

--- matrix.py
+++ matrix.py
@@ -1,11 +1,4 @@
-m1 = [[1,2,3], [4,5,6]]
-m2 = [[1,2], [3,4], [5,6]]
-m3 = [[1,2,3], [3,2,1], [2,1,3]]
-m4 = [[4,5,6], [6,5,4], [4,6,5]]
-m5 = [[101,102,103], [103,104,105], [105, 106, 107]]
-m6 = [[201,202,203], [204,205,206], [207,208,209]]
-
-def mat_add(M1, M2):
+def add_matrices(M1, M2):
     """
     input: M1 and M2 are matrics of same dimension
     output: matrix of the same dimension as M1 and M2
@@ -17,41 +10,63 @@ def mat_add(M1, M2):
     row = len(M1)
     col = len(M1[0])
     
-    ans = [[0] * col] * row
+    ans = [[0 for j in range(col)] for i in range(row)]
     #set elements
     for i in range(row):
-        present_row = []
         for j in range(col):
-            present_row.append(M1[i][j] + M2[i][j])
-        #set the rows one by one
-        ans[i] = present_row
+            ans[i][j] = M1[i][j] + M2[i][j]
 
     return ans 
 
-def mat_sub(M1, M2):
+def subtract_matrices(M1, M2):
     """
     input: M1 and M2 are matrics of same dimension
     output: matrix of the same dimension as M1 and M2
     assumption: M1 + M2 is defined
     
-    returns matrix subtraction of the input matrices
+    returns matrix subtraction of the input matrices;
+    M1 - M2
     """
     #get row and col of input matrices
     row = len(M1)
     col = len(M1[0])
     
-    ans = [[0] * col] * row
+    ans = [[0 for j in range(col)] for i in range(row)]
     #set elements
     for i in range(row):
-        present_row = []
         for j in range(col):
-            present_row.append(M1[i][j] - M2[i][j])
-        #set the rows one by one
-        ans[i] = present_row
+            ans[i][j] = M1[i][j] - M2[i][j]
 
     return ans 
 
-def mat_merge(A, B, C, D):
+def product_matrices(M1, M2):
+    """
+    input: m1(matrix mxn) ; m2(matrix nxp)
+    output: matrix (mxp)
+    assumption: matrix multiplication of input matrices
+    is defined
+    
+    returns matrix product of two input matrices using Gaussian 
+    algorithm
+    """
+    #get m,n,p
+    m = len(M1)
+    n = len(M1[0]) #length of first element of the grid
+    p = len(M2[0])
+    #initialize ans list it should be mxp matrix
+    ans = [[0 for j in range(p)] for i in range(m)]
+    #now set values in the ans matrix
+    for i in range(m):
+        for j in range(p):
+            #each element is obtained as scalar product in input matrices
+            scal_product = 0
+            for k in range(n):
+                scal_product += M1[i][k] * M2[k][j]
+            ans[i][j] = scal_product                
+    
+    return ans
+
+def merge_matrices(A, B, C, D):
     """
     input: A, B, C, D are all matrices. 
     output: a matrix
@@ -76,7 +91,7 @@ def mat_merge(A, B, C, D):
     for i in range(row):
         present_row = []
         for j in range(col):
-            if i < row_a and j < row_a:
+            if i < row_a and j < col_a:
                 present_row.append(A[i][j])
             elif i < row_a and j>= col_a:
                 present_row.append(B[i][j - col_a])
@@ -89,67 +104,57 @@ def mat_merge(A, B, C, D):
         ans[i] = present_row
 
     return ans        
-                
-print mat_merge(m3, m4, m5, m6)                                       
-                
-                
-    
 
-def mat_mul(M1, M2):
+def split_list(L, n):
     """
-    input: m1(matrix mxn) ; m2(matrix nxp)
-    output: matrix (mxp)
-    assumption: matrix multiplication of input matrices
-    is defined
-    
-    returns matrix product of two input matrices using Gaussian
-    algorithm
+    input: L is a list and n is an integer
+    output: a tuple of two lists
+
+    splits the list into two parts. First part is of length n
     """
-    #get m,n,p
-    m = len(M1)
-    n = len(M1[0]) #length of first element of the grid
-    p = len(M2[0])
-    #initialize ans list it should be mxp matrix
-    ans = [[0]* p] * m
-    #now set values in the ans matrix
-    for i in range(m):
-        #list to hold values for scalar product in each row
-        row = []
-        for j in range(p):
-            #each element is obtained as scalar product in input matrices
-            scal_product = 0
-            for k in range(n):
-                scal_product += M1[i][k] * M2[k][j]
-            #append to the row
-            row.append(scal_product)
-        
-        #set the row in ans list to be equal to this row
-        ans[i] = row
-    
-    return ans
+    return L[:n], L[n:]
 
+#print split_list([1,2,3,4,5,6], 3)
 
-def test_matrix_multiply(f):
-    expected = [[[22, 28], [49, 64]], [[28, 33, 29], [28, 31, 31], [26, 33, 31]]]
-    if f(m1, m2) == expected[0] and f(m3,m4) == expected[1]:
-        print str(f), ": tests passed"
-    else:
-        print str(f), ": tests failed; review code"
-
-test_matrix_multiply(mat_mul)
-
-def mat_strass(M1, M2):
+def split_matrix(X):
     """
-    input: m1(matrix mxn) ; m2(matrix nxp)
-    output: matrix (mxp)
-    assumption: matrix multiplication of input matrices
-    is defined
+    input: X is a matrix
+    output: a list of four matrices
     
-    returns matrix product of two input matrices using Strassen's
-    algorithm
+    returns a tuple of four matrices which form big matrix X
     """
-    if len(M1) <=2 and len(M2) <= 2:
-        return mat_mul(M1, M2)
-    else:
-        #get A,B,C,D and E,F,G,H
+    #get dimensions of X
+    row = len(X)
+    col = len(X[0])
+    #get dimensions of top-left matrix
+    row_a = row/2
+    col_a = col/2
+    #initialize sub-matrices
+    A, B, C, D = [], [], [], []
+    #set rows to sub-matrices
+    for i in range(row):
+        if i < row_a:
+            A.append(X[i][:col_a])
+            B.append(X[i][col_a:])
+        else:
+            C.append(X[i][:col_a])
+            D.append(X[i][col_a:])
+
+    print "A:", A
+    print "B:", B
+    print "C:", C
+    print "D:", D
+
+    return A, B, C, D    
+
+
+m3 = [[1,2,3], [3,2,1], [2,1,3]] # 3 x 3
+m4 = [[4,5,6], [6,5,4], [4,6,5]] # 3 x 3
+m5 = [[101,102,103], [103,104,105], [105, 106, 107]] # 3 x 3
+m6 = [[201,202,203], [204,205,206], [207,208,209]] # 3 x 3
+m7 = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]
+
+print split_matrix(m7)
+
+
 
